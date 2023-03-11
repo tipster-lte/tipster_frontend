9ac3bd8311a8a2bc3a8bded6d22ec3dc92c393dd
cuuj69 <mensahjefferson69@gmail.com>
2023-03-11 15:11:24 +0000

    environment configuration

---
+++ tipster-fe/webpack.config.js
@@ -0,0 +1,23 @@
+// webpack.config.js
+
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.ts', // <-- change extension to .ts
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js']
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        loader: 'ts-loader',
+        exclude: /node_modules/
+      }
+    ]
+  }
+};
